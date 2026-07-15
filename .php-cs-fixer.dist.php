0238141e5f0e1e64e55c81f6b337c39dbdd64319
ynbagency <ynb.development@gmail.com>
2026-07-15 11:44:51 +0100

    update

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([__DIR__ . '/src', __DIR__ . '/tests']);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(false)
+    ->setRules([
+        '@PSR12' => true,
+    ])
+    ->setFinder($finder);
